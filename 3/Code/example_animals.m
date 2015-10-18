9e1ba08f558c59392125c22913ed6e4d9ef49be1
Tony Li <tonyli.lives@gmail.com>
2015-10-18 15:26:13 -0700

    1.2

--- 3/Code/example_animals.m
+++ 3/Code/example_animals.m
@@ -2,8 +2,19 @@
 load animals.mat
 
 %% K-Means clustering
-K = 5;
-model = clusterKmeans(X,K);
+% K = 5;
+% model = clusterKmeans(X,K);
+% 
+% for k = 1:max(model.clusters)
+%     fprintf('Cluster %d: ',k);
+%     fprintf('%s ',animals{model.clusters==k});
+%     fprintf('\n');
+% end
+
+%% Density-Based Clustering
+eps = 13;
+minPts = 3;
+model = clusterDBcluster(X,eps,minPts);
 
 for k = 1:max(model.clusters)
     fprintf('Cluster %d: ',k);
