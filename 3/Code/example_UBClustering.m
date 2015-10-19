afc717fa23bc2fe3029e86fd4b7faa6071169690
Tony Li <tonyli.lives@gmail.com>
2015-10-18 20:27:58 -0700

    1.3

--- 3/Code/example_UBClustering.m
+++ 3/Code/example_UBClustering.m
@@ -3,15 +3,20 @@ load clusterData.mat
 
 %% UBClustering
 K = 4;
-model = clusterUBClustering(X,K,20);
+nModels = 20;
+model = clusterUBClustering(X,K,nModels);
 cluster = model.clusters;
 
 %% Make plot
-clf;hold on;
+clf;
+hold on;
+
 colors = getColorsRGB;
 symbols = getSymbols;
+
 h = plot(X(cluster==0,1),X(cluster==0,2),'.');
 set(h,'Color',[0 0 0]);
+
 for k = 1:K
     h = plot(X(cluster==k,1),X(cluster==k,2),'.');
     set(h,'Color',colors(k,:),'Marker',symbols{k},'MarkerSize',12);
