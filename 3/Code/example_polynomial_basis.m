32880b2bab8a73686fe012a10625d4c737b01ed2
Tony Li <tonyli.lives@gmail.com>
2015-10-20 01:38:33 -0700

    3.3

--- 3/Code/example_polynomial_basis.m
+++ 3/Code/example_polynomial_basis.m
@@ -1,24 +1,41 @@
 % Load data
 load basisData.mat
 
-% Create the figure
-figure(1);
+split = 250;
 
-degree = 8;
+% Split the data into training and test sets
+xTraining = X(1:split,:);
+yTraining = y(1:split,:);
+xTest = X(split+1:end,:);
+yTest = y(split+1:end,:);
+
+degree = 20;
+
+ASETraining = zeros(degree+1,1);
+ASETest = zeros(degree+1,1);
 
 for j = 0:degree
-    % Draw and label each subplot
-    subplot(3,3,j+1);
-    plot(X,y,'b.');
-    string = sprintf('degree %i', j);
-    title(string);
-    hold on;
-    
     % Fit least-squares with polynomial basis estimator
-    model = leastSquaresBasis(X,y,j);
+    model = leastSquaresBasis(xTraining,yTraining,j);
 
-    % Draw model prediction
-    Xsample = [min(X):.1:max(X)]';
-    yHat = model.predict(model,Xsample);
-    plot(Xsample,yHat,'g-'); 
+    % Training error
+    yHat = model.predict(model,xTraining);
+    ASETraining(j+1) = sum((yHat - yTraining).^2) / 250;
+    
+    % Validation error
+    yHat = model.predict(model,xTest);
+    ASETest(j+1) = sum((yHat - yTest).^2) / 250;
+
+    fprintf('Degree : %i \n',j);
+    fprintf('Training error: %f \n',ASETraining(j+1));
+    fprintf('Validation error: %f \n',ASETest(j+1));
 end
+
+plot(0:degree,ASETraining,'-go');
+plot(0:degree,ASETest,'-bx');
+title('Training Error vs Test Error');
+legend('Test Error','Training Error');
+hold on;
+
+% plot(0:degree, abs(ASETraining - ASETest)./ASETraining, '-r*');
+% legend('(trainMSE - validationMSE) / trainMSE');
