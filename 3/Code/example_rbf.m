bcab275baa532aa1177cd6cd1dfdd80bbeec6907
Tony Li <tonyli.lives@gmail.com>
2015-10-21 16:42:09 -0700

    4.2

--- 3/Code/example_rbf.m
+++ 3/Code/example_rbf.m
@@ -1,11 +1,11 @@
-% Load data
+%% Load Data
 warning off all;
 close all;
 clear all;
 load nonLinearData.mat;
 [n,d] = size(X);
 
-% Plotting Code
+%% Plotting Code
 plot(X,y,'b.');
 hold on;
 plot(Xtest,ytest,'g.');
@@ -14,37 +14,59 @@ yl = ylim;
 Xvals = [xl(1):.1:xl(2)]';
 pause(.1);
 
-yHatLowest = 100;
+%% Train
+
+% Split the data into training and test sets
+split = 50;
+
+xTraining = X(1:split,:);
+yTraining = y(1:split,:);
+xValidation = X(split+1:end,:);
+yValidation = y(split+1:end,:);
+
+errorLowest = 100;
 
 % Display result of fitting with RBF kernel
 for sigma = 2.^[3:-1:-4]
-    for lambda = 2.^[3:-1:-4]
-        %% Train on X, test on Xtest
-        model = leastSquaresRBF(X,y,sigma,lambda);
-        yHat = model.predict(model,Xtest);
+    for lambda = 2.^[-12:2]
+        % Train on xTraining, validate on xValidation
+        model = leastSquaresRBF(xTraining,yTraining,sigma,lambda);
+        yHat = model.predict(model,xValidation);
+        error = mean(abs(yHat-yValidation));
         
-        error = mean(abs(yHat-ytest));
-        
-        if error < yHatLowest
-            yHatLowest = mean(abs(yHat-ytest));
+        % Determine if error is lower
+        if error < errorLowest
+            errorLowest = error;
             sigmaLowest = sigma;
             lambdaLowest = lambda;
         end
         
         fprintf('Test error sigma = %f, lambda = %f, is %f\n',sigma,lambda,error);
-
-        %% Plotting Code
-        figure(1);clf;
-        plot(X,y,'b.');
-        hold on;
-        plot(Xtest,ytest,'g.');
-        yvals = model.predict(model,Xvals);
-        plot(Xvals,yvals,'r-');
-        legend({'Train','Test'});
-        ylim(yl);
-        title(sprintf('RBF Basis (sigma = %f)',sigma));
-        pause(.5);
     end
 end
 
-fprintf('Lowest test error sigma = %f, lambda = %f, is %f\n',sigmaLowest,lambdaLowest,yHatLowest);
+%% Test
+
+fprintf('Best sigma = %f\n',sigmaLowest);
+fprintf('Best lambda = %f\n',lambdaLowest);
+
+% Train on full X
+model = leastSquaresRBF(X,y,sigmaLowest,lambdaLowest);
+% Test on Xtest
+yHat = model.predict(model,Xtest);
+% Get the final error
+error = mean(abs(yHat-ytest));
+
+fprintf('Test error using best is %f\n',error);
+
+%% Plot Results
+figure(1);
+clf;
+plot(X,y,'b.');
+hold on;
+plot(Xtest,ytest,'g.');
+yvals = model.predict(model,Xvals);
+plot(Xvals,yvals,'r-');
+legend({'Train','Test'});
+ylim(yl);
+title(sprintf('RBF Basis (sigma = %f, lambda = %f)',sigmaLowest,lambdaLowest));
