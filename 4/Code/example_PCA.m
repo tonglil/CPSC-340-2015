06a0c92370337a3560ec9677b729be2cb45798ce
Tony Li <tonyli.lives@gmail.com>
2015-11-11 00:51:03 -0800

    2.2

--- 4/Code/example_PCA.m
+++ 4/Code/example_PCA.m
@@ -15,3 +15,17 @@ imagesc(Z);
 figure(2);
 scatter(Z(:,1),Z(:,2),'.');
 gname(animals);
+
+%% Compression
+for k = 1:3
+    W = V(:,1:k)';
+    Z = X*W'; % Z is n-by-k
+
+    ZW = Z*W;
+    compressionRatio = norm(X - ZW,'fro')^2 / norm(X,'fro')^2
+end
+
+singularValues = diag(S(:,1:n));
+
+% Compression ratio for k
+cr = 1 - cumsum(singularValues.^2 ./ sum(singularValues.^2))
